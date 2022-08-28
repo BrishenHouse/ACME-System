8fd9ff87dcf44a05c6232dccde9e576a8476964d
Brishen House <brishenhouse123@gmail.com>
2022-08-28 13:25:43 -0400

    whatFor() method completed

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -7,11 +7,15 @@ public class HelloWorld {
 		System.out.println("Hello from Eclipse");
 		System.out.println("Brish is working on Eclipse");
 		heyNow();
-
+		whatFor("hello");	
 	}
 
 	public static void heyNow() {
 		String msg = "hey now!";
 		System.out.println(msg);
+	}
+	
+	public static void whatFor(String msg) {
+		System.out.println(msg + " stringArg, what for?");
 	}
 }
